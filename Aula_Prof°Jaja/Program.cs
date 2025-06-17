a32f397b911c1f923981f28caba1ea54ea0874c6
Abel Garcia <119456222+Abel2007u@users.noreply.github.com>
2025-06-16 23:04:15 -0300

    Atualizacao nas Pastas e organização

--- Aula_Prof°Jaja/Program.cs
+++ Aula_Prof°Jaja/Program.cs
@@ -26,7 +26,42 @@ using ExercLPAulasJaja;
 //ConversaoTipo.Main(args);
 //ExemploClassMatematica.Main(args);
 //SubstringEIndexOf.Main(args);
-EspeciaisCaracteresString.Main(args);
+//EspeciaisCaracteresString.Main(args);
+
+/*AULA 06'06 SEXTA */
+//Vetor.Main(args);
+//ForEachExemplo02.Main(args);
+//CriacaoVetorEPS.Main(args);
+//SystemLinq.Main(args);
+//ForEachSort2.Main(args);
+//Matriz2D_Ex1.Main(args);
+
+
+/*AULA 09'06 SEG */
+//AlterarElementosMatriz.Main(args);
+//ForEachMatriz2D.Main(args);
+//MatrizLoopFor.Main(args);
+//ListArMat01_Array1D.Main(args);
+//ListArMat02_MaiorValorArray1D.Main(args);
+//CriandoMetodos.Main(args);
+//CriandoMetodosComParametros.Main(args);
+//MetodosComMultiplosParametros.Main(args);
+//MetodosComValoresPadrao.Main(args);
+//MetodoRetornoValor.Main(args);
+//MetodoRetornar2Valores.Main(args);
+//MetodoArgumentosNomeados.Main(args);
+//SobrecargaDeMetodo.Main(args);
+
+/*AULA 11'06 TER */
+//SobrecargaExemplo2Entrada.Main(args); //Com entrada do user
+//Professor.Main(args);
+//Computador.Main(args);
+//ProfessorEx2.Main(args);
+//FrutaGetAtrib.Main(args);
+
+/*AULA 16'06 SEG Exercícios by Gemini*/
+//ListArMat02_MaiorValorArray1D.Main(args);
+ListArMat03_InverteString.Main(args);
 
 //Também tem o -- decremento ou ++incremento
 //E o modulo % que traz o resto da divisão;
